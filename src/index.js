b53c232cbfdad422344dded7705f0abca09a73af
Wahyu Ramadan <wramadan1203@gmail.com>
2022-11-21 11:22:17 +0700

    setup index

---
+++ src/index.js
@@ -0,0 +1,40 @@
+import express from 'express';
+import helmet from 'helmet';
+import xss from 'xss-clean';
+import cors from 'cors';
+import compression from 'compression';
+import * as dotenv from 'dotenv';
+import routesV1 from './routes/v1/index.js';
+
+dotenv.config();
+
+const { NODE_PORT } = process.env;
+
+const app = express();
+
+// Set security HTTP headers
+app.use(helmet());
+
+// Parse JSON request body
+app.use(express.json());
+
+// Parse URLEncoded request body
+app.use(express.urlencoded({ extended: false }));
+
+// Sanitize request data
+app.use(xss());
+
+// Gzip compression
+app.use(compression());
+
+// Enable cors
+app.use(cors());
+app.options('*', cors());
+
+app.use('/v1', routesV1);
+
+app.listen(NODE_PORT, () => {
+  console.log(`App listen on port ${NODE_PORT}.`);
+});
+
+export default app;

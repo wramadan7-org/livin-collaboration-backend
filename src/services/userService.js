2df47d2110eaf343c6ef6dd08beb8416c2d22509
Wahyu Ramadan <wramadan1203@gmail.com>
2022-11-22 13:34:52 +0700

    try to get error

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,7 +1,7 @@
 import db from '../config/db.js';
 
 const getAllUserService = async () => {
-  const users = await db.pool.query('SELECT * FROM users WHERE email = \'wramadan1203@gmail.com\'');
+  const users = await db.pool.query('SELECT * FROM users WHERE email = \'wramadan1203@gmail.coms\'');
 
   return users;
 };

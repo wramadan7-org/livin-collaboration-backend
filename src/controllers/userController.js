985b5e42b4d412087a4e03f0a2200012743ef7d0
Wahyu Ramadan <wramadan1203@gmail.com>
2022-11-23 15:35:25 +0700

    create Promise catchAsync for controller function

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,18 +1,15 @@
 import httpStatus from 'http-status';
 import BaseError from '../middlewares/baseError.js';
+import catchAsync from '../helpers/catchAsync.js';
 import userService from '../services/userService.js';
 
-const getAllUserController = async (req, res, next) => {
-  try {
-    const users = await userService.getAllUserService();
+const getAllUserController = catchAsync(async (req, res) => {
+  const users = await userService.getAllUserService();
 
-    if (users.rowCount <= 0) throw new BaseError('User is empty.', httpStatus.NOT_FOUND);
+  if (users.rowCount <= 0) throw new BaseError('User is empty.', httpStatus.NOT_FOUND);
 
-    return res.sendWrapped('List of all users', null, httpStatus.OK);
-  } catch (error) {
-    next(error);
-  }
-};
+  return res.sendWrapped('List of all users', null, httpStatus.OK);
+});
 
 export default {
   getAllUserController,

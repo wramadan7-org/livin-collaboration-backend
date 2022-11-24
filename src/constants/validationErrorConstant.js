1212b1c98fea372e14045b29ab63cc3c1635ead4
Wahyu Ramadan <wramadan1203@gmail.com>
2022-11-24 16:43:33 +0700

    create constant error message validation

---
+++ src/constants/validationErrorConstant.js
@@ -0,0 +1,12 @@
+const validationErrorMessage = {
+  FIELD_CANNOT_BE_EMPTY: 'Field can\'t be empty.',
+  EMAIL_INCORRECT: 'Email is incorrect.',
+  MIN_LENGTH: 'Must be at least',
+  MAX_LENGTH: 'Must be at most',
+  CONTAIN_NUMBER: 'Must contain a number.',
+  ONLY_CONTAIN_NUMBER: 'Only contain number.',
+  GENDER: 'Only contain \'male\' or \'female\'',
+  ACCOUNT_TYPE: 'Only contain \'private\' or \'public\'',
+};
+
+export default validationErrorMessage;

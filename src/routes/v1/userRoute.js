7b5008d04c4d745e50de5e62d14f289a0818a310
Wahyu Ramadan <wramadan1203@gmail.com>
2022-11-24 16:45:14 +0700

    use middleware validator to validate request when create user

--- src/routes/v1/userRoute.js
+++ src/routes/v1/userRoute.js
@@ -1,8 +1,13 @@
 import { Router } from 'express';
 import userController from '../../controllers/userController.js';
+import validator from '../../middlewares/validator.js';
+import userValidation from '../../validation/userValidation.js';
 
 const router = Router();
 
+const { createUserValidation } = userValidation;
+
+router.post('/', validator(createUserValidation), userController.createUserController);
 router.get('/', userController.getAllUserController);
 router.get('/:idUser', userController.getUserByIdController);
 

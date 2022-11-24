2d6186be4622b418b740c4fdd31f4b5a54be2a1f
Wahyu Ramadan <wramadan1203@gmail.com>
2022-11-24 14:45:42 +0700

    create description of function in service

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,11 +1,20 @@
 import db from '../config/db.js';
 
+/**
+ * Service to get all user from DB
+ * @returns Array
+ */
 const getAllUserService = async () => {
   const users = await db.pool.query('SELECT * FROM users');
 
   return users;
 };
 
+/**
+ * Service to get user by Id from DB
+ * @param {Number} id
+ * @returns Object
+ */
 const getUserByIdService = async (id) => {
   const user = await db.pool.query(`SELECT * FROM users WHERE id = ${id}`);
 
